db96ad413c47361adcaf7f769ad71040d2385659
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-08-08 10:46:50 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Net;
+//Exercicio 8
+namespace Aula_Presencial_8_80
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ip; 
+
+            Console.Write("Entre com o IP: ");
+            ip =  Int32.Parse(Console.ReadLine());
+
+            //Classe A
+
+            if ((ip >= 0 )  && (ip <= 127)) {
+
+                Console.WriteLine("Ip classe A");
+            }
+
+            //Classe B
+            else if ((ip >= 128) && (ip <= 192)){
+
+
+              Console.WriteLine("Ip classe B");
+            }
+            //Classe C
+            else if ((ip >= 192) && (ip <= 223)) {
+
+                Console.WriteLine("Ip classe C");
+            }
+
+            //Classe D
+            else if ((ip >= 224) && (ip <= 239)) {
+
+                Console.WriteLine(("Ip Classe D"));
+            }
+
+            //Classe E
+            else if ((ip >= 248) && (ip <= 1255)) {
+
+                Console.WriteLine("Ip Classe E");
+            }
+            else {
+                Console.WriteLine("Ip invalido");
+            }
+
+
+        }
+
+
+  
+    }
+}
